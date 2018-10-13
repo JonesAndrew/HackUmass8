28b6fc5837c1a341eacad60aa1de04bb174f9431
JonesAndrew <andrew.joe.jones@gmail.com>
2018-10-13 11:41:06 -0400

    circle change

--- components/circle.lua
+++ components/circle.lua
@@ -8,8 +8,9 @@ function Circle:new(gameobject, radius, segments)
 
     self.radius = radius
     self.segments = segments
+    self.style = "line"
 end
 
 function Circle:render()
-    love.graphics.circle("line", self.gameobject.x, self.gameobject.y, self.radius, self.segments)
+    love.graphics.circle(self.style, self.gameobject.x, self.gameobject.y, self.radius, self.segments)
 end
