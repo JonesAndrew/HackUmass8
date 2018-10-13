c5d2b04f2543a742a83c7b5fdc419e7bf5c2a609
Alexander Vukovic <alexvukovic96@gmail.com>
2018-10-13 11:44:10 -0400

    reticle & target

--- gameobjects/dontmiss/reticle.lua
+++ gameobjects/dontmiss/reticle.lua
@@ -6,8 +6,25 @@ Gameobject.add("reticle", Reticle)
 function Reticle:new(gameobject, text)
     Gameobject.Gameobject.new(self, gameobject)
 
-    self.offset_x = 0
-    self.offset_y = 0
-    self.text = text
-    self.binding = nil
+    self.input = baton.new {
+      controls = {
+        left = {'key:left', 'key:a', 'axis:leftx-', 'button:dpleft'},
+        right = {'key:right', 'key:d', 'axis:leftx+', 'button:dpright'},
+        up = {'key:up', 'key:w', 'axis:lefty-', 'button:dpup'},
+        down = {'key:down', 'key:s', 'axis:lefty+', 'button:dpdown'},
+        a = {'key:z', 'button:a'},
+        b = {'key:x', 'button:a'},
+        c = {'key:c', 'button:a'},
+        d = {'key:v', 'button:a'},
+      },
+      pairs = {
+        move = {'left', 'right', 'up', 'down'}
+      },
+      joystick = love.joystick.getJoysticks()[index],
+    }
+
+    self.inner = self:add_component(Component.get('circle')(self, 12))
+    self.outer = self:add_component(Component.get('circle')(self, 48))
 end
+
+function Reticle:fire(gameobject)
