dedd46c8f4df579739d4e46245c1a0835ed2e1b5
Alexander Vukovic <alexvukovic96@gmail.com>
2018-10-13 00:41:00 -0400

    implemented sway
    a spectre is haunting europe

--- components/sway.lua
+++ components/sway.lua
@@ -7,16 +7,22 @@ function Sway:new(gameobject, position)
     Component.Component.new(self, gameobject)
 
     self.destination = {(gameobject.x + love.math.random(-48,48)), (gameobject.y + love.math.random(-27,27))}
+    self.moveflag = true;
 end
 
 function Sway:checkpoints(vectora, vectorb)
-	local epsilon = 3
-	if ((math.abs(vectora[1] - vectorb[1]) =< 3) and (math.abs(vectora[1] - vectorb[2]))
+	return ((math.abs(vectora[1] - vectorb[1]) <= 3) and (math.abs(vectora[1] - vectorb[2]) <= 3))
 
 function Sway:update(dt)
-	if (self.destination[1] = gameobject.x gameobject.y})
-	local vector = {gameobject.x - destination[1], gameobject.y - destnation[2]}
-	local 
+	if (checkpoints({gameobject.x, gameobject.y}, destination))
+		self.destination = {(gameobject.x + love.math.random(-48,48)), (gameobject.y + love.math.random(-27,27))}
+	else if (!moveflag) 
+		return
+	local vector = {gameobject.x - destination[1], gameobject.y - destination[2]}
+	local perp = {-vector[2], vector[1]}
+	gameobject.vel_x += perp[1] * dt
+	gameobject.vel_y += perp[2] * dt
+	return
 
 
 
