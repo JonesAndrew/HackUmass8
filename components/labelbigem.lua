28db9bbdde84bb74a7932b853e2df732a2fc8b51
Chester Moses <c_moses3@post.massbay.edu>
2018-10-13 14:23:00 -0400

    Added more labels

--- components/labelbigem.lua
+++ components/labelbigem.lua
@@ -1,11 +1,11 @@
 local Component = require "components/index"
 
-local Label = Component.Component:extend()
-Component.add("label", Label)
+local LabelBigEm = Component.Component:extend()
+Component.add("labelbigem", LabelBigEm)
 
-ptboldsm = love.graphics.newFont("fonts/PT_Sans-Bold.ttf", 12)
+ptboldem = love.graphics.newFont("fonts/PT_Sans-BoldItalic.ttf", 20)
 
-function Label:new(gameobject, text)
+function LabelBigEm:new(gameobject, text)
     Component.Component.new(self, gameobject)
 
     self.offset_x = 0
@@ -14,12 +14,12 @@ function Label:new(gameobject, text)
     self.binding = nil
 end
 
-function Label:render()
+function LabelBigEm:render()
     if self.binding then
         self.text = self.binding()
     end
 
 	love.graphics.setColor(colors[4])
-	love.graphics.setFont(ptboldsm)
+	love.graphics.setFont(ptboldem)
     love.graphics.printf(self.text, self.gameobject.x - 200 + self.offset_x, self.gameobject.y + self.offset_y, 400, 'center')
 end
--- scenes/title.lua
+++ scenes/title.lua
@@ -12,13 +12,31 @@ function Title:new()
     self:add_gameobject(name)
 	
 	local subtitle = Gameobject.get("basic")()
-	subtitle:add_component(Component.get("label")(subtitle, "The Soviet Spy Training Programme"))
+	subtitle:add_component(Component.get("labelbig")(subtitle, "The Soviet Spy Training Programme"))
 	self:add_gameobject(subtitle)
+	
+	local warning = Gameobject.get("basic")()
+	warning:add_component(Component.get("labelbigem")(warning, "Confidential Material"))
+	self:add_gameobject(warning)
+	
+	local disclaimer = Gameobject.get("basic")()
+	disclaimer:add_component(Component.get("label")(disclaimer, "If you are not authorized to view this programme,\nyou must report to your superior immediately"))
+	self:add_gameobject(disclaimer)
+	
+	local start = Gameobject.get("basic")()
+	start:add_component(Component.get("labeldisplaysm")(start, "Press any key to start"))
+	self:add_gameobject(start)
 
     name.x = 240
     name.y = 10
 	subtitle.x = 240
 	subtitle.y = 120
+	warning.x = 240
+	warning.y = 150
+	disclaimer.x = 240
+	disclaimer.y = 200
+	start.x = 240
+	start.y = 240
 end
 
 return Title
