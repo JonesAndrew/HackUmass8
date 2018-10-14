7837c2256d25d40a41e70bdef9ba5a035ecb3719
Jonathan Julien <jjulien@umass.edu>
2018-10-14 02:12:58 -0400

    Update scenes/board.lua

--- scenes/board.lua
+++ scenes/board.lua
@@ -53,6 +53,13 @@ local cave = {
     'cavegame'
 }
 
+local valkyrie = {
+    "Flight of the Valkyries",
+    "Escape is the most important part of an agent's mission!",
+    "In this exercise, you are pairs of pilots. One pair of pilots, in smaller planes, must escape their pursuers for 30 seconds. The pursuers must eliminate the targets within the time limit. But beware! The pursuers are armed with cannons!",
+    "Use the control stick to steer the plane! Pursuers, use A to fire your cannon!",
+    'spyplane'
+}
 function Board:new()
     Scene.new(self)
     self.piecies = {}
